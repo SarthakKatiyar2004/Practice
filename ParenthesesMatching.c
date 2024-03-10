b0845b30e19c3a18a86f9c80daf4f2fddc101198
Sarthak Katiyar <150513632+SarthakKatiyar2004@users.noreply.github.com>
2024-03-10 23:02:35 +0530

    Update ParenthesesMatching.c
    made some changes, well... on the good side it's taking the input without generating any error but it's also only taking the input without generating any thing.

--- ParenthesesMatching.c
+++ ParenthesesMatching.c
@@ -4,24 +4,97 @@
 
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-int top = -1;
-int stack[100];
-char string[100];
+#define MAX_LENGTH 1000
+
+char *pString = NULL; //creating a string pointer and initializing it to NULL
+char temp[MAX_LENGTH];
+
+struct Stack{
+    int top;
+    int capacity;
+    char *pArray;
+};
+
+struct Stack *pCreatestack(unsigned capacity){
+    struct Stack *pStack = (struct Stack*)malloc(sizeof(struct Stack));
+    pStack -> top = -1;
+    pStack -> capacity = capacity;
+    pStack -> pArray = (char *)malloc(pStack -> capacity * sizeof(char));
+}
+
+int isPair(char char1, char char2){
+    if (char1 == '(' && char2 == ')'){
+        return 1;
+    }
+    if (char1 == '{' && char2 == '}'){
+        return 1;
+    }
+    if (char1 == '[' && char2 == ']'){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+    
+}
+
+void push(struct Stack *pStack, char char1){
+    if (pStack -> top == pStack -> capacity - 1){
+        printf("Stack Overflow!");
+    }
+    else{
+        scanf("%c", pStack -> pArray + ++(pStack -> top));
+    }
+}
+
+void pop(struct Stack *pStack, char char2){
+    if (pStack -> top == -1){
+        printf("Stack Underflow!");
+    }
+    else{
+        if(isPair(*(pStack -> pArray + pStack -> top), char2)){
+            (pStack -> top)--;
+        }
+    }
+}
+
 
 int main(){
 
-    printf("Enter a string containing parentheses: ");
-    scanf("%s",string);
+    printf("Enter the expression sting: ");
+    fgets(temp, MAX_LENGTH, stdin); //temp is allocated a memory of MAX_LENGTH in stdin stream
 
-    int i = 0;
-    while (string[i]){
-        if (string[i] == "("
-        || string[i] == "{"
-        || string[i] == "["){
-            push(string[i]);
-            }
+    //now let us say temp has 6 characters + NULL then we only need to work with 6 characters the remaining 994 are garbage
+    //for that we can dynamically allocate a space of 7 characters to *pString
+
+    pString = (char *)malloc(strlen(temp));
+
+    if(pString == NULL){
+            printf("Memory allocation failed!\n");
+            return 1;
     }
+
+    strcpy(pString, temp);
+
+    struct Stack* stack = pCreatestack(strlen(pString));
+
+    while (*pString){
+        
+        if(*pString == '(' || *pString == '{' || *pString == '['){
+            push(stack, *pString);
+        }
+        else{
+            pop(stack, *pString);
+        }
+
+        pString++;
+        
+    }
+
+    printf("%s",pString);
 
     return 0;
 }
