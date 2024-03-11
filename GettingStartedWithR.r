20e9772537ab6897549d53afd6c4852cc37ccab2
Sarthak Katiyar <150513632+SarthakKatiyar2004@users.noreply.github.com>
2024-03-11 23:28:18 +0530

    Create GettingStartedWithR.r
    Beginner exercises from "The book of R"

---
+++ GettingStartedWithR.r
@@ -0,0 +1,28 @@
+#Exercise 2.1 of "The Book of R"
+
+#Question 1
+((6*2.3)+42)/(3^(4.2-3.62))
+
+#Question 3
+(mean(25.2,15,16.44,15.3,18.6)/2)^(0.5)
+
+#Question 4
+log(0.3)
+
+#Question 5
+exp(x = log(0.3))
+
+#Exercise 2.2
+
+#Question 1
+x <- 3^2 + 4^(1/8)
+
+#Question 2
+x <- x/2.33
+print(x)
+
+#Question 3
+y <- -8.2*10^-13
+
+#Question 4
+print(x*y)
