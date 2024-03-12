d0a93cc2aabdf02b03f08a13321fbdef7ca70b59
Sarthak Katiyar <150513632+SarthakKatiyar2004@users.noreply.github.com>
2024-03-12 22:12:37 +0530

    Create CI_LinkedList.c
    currently incomplete

---
+++ CI_LinkedList.c
@@ -0,0 +1,16 @@
+//Implementing singly linked list using struct
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *pNext;
+} *pHead = NULL, *pTemp;
+
+int main(){
+
+    
+
+    return 0;
+}
