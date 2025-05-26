aee5d92edfb68bdf7eb649dcd6825d3bd395410f
Sarthak Katiyar <150513632+SarthakKatiyar2004@users.noreply.github.com>
2025-05-26 18:48:15 +0530

    Update Med_Longest Palindromic Substring.c
    added best method

--- LeetCode Practice/Med_Longest Palindromic Substring.c
+++ LeetCode Practice/Med_Longest Palindromic Substring.c
@@ -10,7 +10,7 @@ int isPalindrome(char* s, int start, int end){
 	return true;
 }
 
-char* longestPossiblePalindrome(char* s){
+char* longestPalindromeSubstring(char* s){
 	int maxLen = 0, startId = 0, n = strlen(s);
 	for (int i = 0; i < n; i++){
 		for (int j = 0; j < n; j++){
@@ -34,7 +34,7 @@ void expandAroundCenter(char* s, int l, int r, int* lout, int* rout){
 	*rout = r-1;
 }
 
-char* longestPossiblePalindrome(char* s){
+char* longestPalindromeSubstring(char* s){
 	int start = 0, end = 0, n = strlen(s);
 
 	for (int i = 0; i < n; i++){
@@ -53,4 +53,46 @@ char* longestPossiblePalindrome(char* s){
 	return res;
 }
 
-/*Best Approach: Manacher's Algorithm*/
+/*Best Approach: Manacher's Algorithm
+Time Complexity = O(N)*/
+char* preprocess(const char* s){
+	int n = strlen(s), j = 0;
+	char* t = (char*)malloc((2*n+4)*sizeof(char));
+	t[j] = '^';
+	t[j++] = '#';
+	for (int i = 0; i < n; i++){
+		t[j++] = s[i];
+		t[j++] = '#';
+	}
+	t[j++] = '#';
+	t[j++] = '$';
+	t[j++] = '\0';
+
+	return t;
+}
+
+char* longestPalindromeSubstring(char* s){
+	char* t = preprocess(s);
+	int n = strlen(t);
+	int *p = (int*)calloc(n, sizeof(int));
+	int center = 0, right = 0;
+
+	for (int i = 1; i < n-1; i++){
+		int mirror = 2*center-i;
+		if (i < right)  {p[i] = (right - i < p[mirror] ? right-i : p[mirror]);}
+		while (t[i + 1 + p[i]] == t[i - 1 - p[i]]) p[i]++;
+		if (i + p[i] > right) {center = i; right = i + p[i]; }
+	}
+
+	int maxLen = 0, centerIndex = 0;
+	for (int i = 1; i < n-1; i++){ if (p[i] > maxLen) maxLen = p[i]; centerIndex = i; }
+	int start = (centerIndex - maxLen)/2;
+
+	char* res = (char*)malloc((maxLen+1)*sizeof(char));
+	strncpy(res, s+start, maxLen);
+	s[maxLen] = '\0';
+
+	free(t);
+	free(p);
+	return s;
+}
