eff9338d711a91cd7923fc5e1db3935608c63239
Sarthak Katiyar <150513632+SarthakKatiyar2004@users.noreply.github.com>
2025-05-26 17:30:29 +0530

    Update Med_Longest Palindromic Substring.c
    Finished adding the better approach.
    
    Now to add the best approach and solve another problem.

--- LeetCode Practice/Med_Longest Palindromic Substring.c
+++ LeetCode Practice/Med_Longest Palindromic Substring.c
@@ -27,6 +27,30 @@ char* longestPossiblePalindrome(char* s){
 /*Better Approach: Expand Around Center
 This approach is based on the fact that every palindrome is based around a character (odd length) or pair (even length)
 Time complexity = O(N^2)*/
+void expandAroundCenter(char* s, int l, int r, int* lout, int* rout){
+	int n = strlen(s);
+	while(l >= 0 && r <= n-1 && s[l] == s[r]) { l--; r++; }
+	*lout = l+1;
+	*rout = r-1;
+}
 
+char* longestPossiblePalindrome(char* s){
+	int start = 0, end = 0, n = strlen(s);
+
+	for (int i = 0; i < n; i++){
+		int l1, l2, r1, r2;
+		expandAroundCenter(s, i, i, l1, r1);
+		expandAroundCenter(s, i, i+1, l2, r2);
+
+		if (r1-l1 > end-start) { start = l1; end = r1;}
+		if (r2-l2 > end-start) { start = l2; end = r2;}
+	}
+	n = end-start+1;
+	char* res = (char*)malloc((n+1)*sizeof(char));
+	strncpy(res, s+start, n);
+	res[n+1] = '\0';
+
+	return res;
+}
 
 /*Best Approach: Manacher's Algorithm*/
