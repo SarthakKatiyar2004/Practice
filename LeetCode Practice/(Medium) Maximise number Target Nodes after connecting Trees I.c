a1e1c2968552e887533d8e2354df04d8e4b9f4d1
Sarthak Katiyar <150513632+SarthakKatiyar2004@users.noreply.github.com>
2025-05-28 22:17:00 +0530

    Made the File. Need to add Code.

---
+++ LeetCode Practice/(Medium) Maximise number Target Nodes after connecting Trees I.c
@@ -0,0 +1,9 @@
+/*
+Given 2 trees we have to find the maximum number of nodes that can be reaches within "k" steps for each node in tree 1
+given that we have to add 1 extra edge from tree 1 to tree 2
+*/
+
+//Brute Force
+int* maxTargetNodes(int** edges1, int edges1Size, int* edges1ColSize, int** edges2, int edges2Size, int* edges2ColSize, int k, int* returnSize) {
+  
+}
